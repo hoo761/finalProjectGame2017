e7fdf5e48f2967c43e378e82936019f222e5c0ad
HS Student <hsstudent@DSISD.local>
2017-04-25 16:09:39 -0500

    Updated

--- fianlProject2048Game/src/com/finalProject2017/game/Game.java
+++ fianlProject2048Game/src/com/finalProject2017/game/Game.java
@@ -1,11 +1,14 @@
 package com.finalProject2017.game;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
@@ -13,8 +16,8 @@ public class Game extends JPanel implements KeyListener, Runnable
 {
 
 	private static final long serialVersionUID = 1L;
-	public static final int WIDTH = 400;
-	public static final int HEIGHT = 630;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 900;
 	public static final Font main = new Font("Times New Roman", Font.PLAIN, 28);
 	private Thread game;
 	private boolean running;
@@ -24,6 +27,7 @@ public class Game extends JPanel implements KeyListener, Runnable
 		setFocusable(true);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		addKeyListener(this);
+		setBackground(Color.BLUE);
 	}
 	
 	public void update()
@@ -42,6 +46,12 @@ public class Game extends JPanel implements KeyListener, Runnable
 		{
 			update();
 		}	
+	}
+	
+	public void paintComponent(Graphics g) 
+	{
+		Graphics2D g2d = (Graphics2D) g;
+		
 	}
 	
 	public synchronized void start()
